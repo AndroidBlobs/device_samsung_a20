77cd1e415edfae7d72cdd8252a4f2416bf5a5453
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-04 07:46:06 +0000

    a20ub-user 9 PPR1.180610.011 A205GUBU4ASK2 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_a20.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from a20 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := a20
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_NAME := lineage_a20
+PRODUCT_MODEL := SM-A205G
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+TARGET_VENDOR := samsung
+TARGET_VENDOR_PRODUCT_NAME := a20
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="a20ub-user 9 PPR1.180610.011 A205GUBU4ASK2 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := samsung/a20ub/a20:9/PPR1.180610.011/A205GUBU4ASK2:user/release-keys
